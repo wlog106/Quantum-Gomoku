ff108132f0fb776001d15c2f13cf1ffb24918d0c
wlog106 <xuanhao8765@gmail.com>
2025-11-24 01:25:29 +0800

    new: add command format info

--- lib/command.h
+++ lib/command.h
@@ -1,2 +1,9 @@
 #define C_create_new_account 100
 #define C_login_to_server 101
+
+
+/*
+    command format>
+        C_create_new_accout:    100:int  username:str passwd_hash:str
+        C_login_to_server:      101:int  username:str passwd_hash:str 
+*/
