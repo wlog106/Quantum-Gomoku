33e0e0ebb1f84a80a8ecda34fc5b10ee1277c50c
wlog106 <xuanhao8765@gmail.com>
2025-11-30 23:02:42 +0800

    update: server's header file

--- server_/server_.h
+++ server_/server_.h
@@ -6,23 +6,17 @@
 #include "dispatcher/dispatcher.h"
 #include "logic/logic.h"
 #include "parser/parser.h"
+#include "utils/utils.h"
+#include "mariadb_api/mariadb_api.h"
 
-#include <assert.h>
+#define MAX_JOB 10
+#define MAX_EVENT 50
+
+#define evfd events[i].data.fd
+#define evtype events[i].events
 
 typedef enum{
     ACT_SEND,
     DB_LOGIN,
     DB_SIGNUP,
 } Action_t;
-
-void Epoll_ctl(int epfd, int op, int fd, struct epoll_event *ev);
-
-void Socketpair(int domain, int type, int protocol, int sv[1]);
-
-conn_t *get_user(int fd, std::map<int, conn_t*> &fd_to_conn);
-
-pid_t Fork();
-
-void Close(int fd);
-
-void Execv(const char *path, char *const argv[]);
