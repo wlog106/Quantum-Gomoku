9efa5f9748afb9943c6dfd30cd372f43fa3d94a1
soondro2266 <100667910+soondro2266@users.noreply.github.com>
2025-12-04 14:41:18 +0800

    add: waiting ui instruction

--- client/lib/ui/src/waiting_room_ui.cpp
+++ client/lib/ui/src/waiting_room_ui.cpp
@@ -6,18 +6,21 @@ void waiting_room_ui(){
     cout << "\x1b[1mWaiting Room" << endl;
     cout << "Room id: " << waiting_room_id << endl;
     cout << "════════════════════════════════════════" << endl;
-    cout << "\x1b[0m\x1b[40;37mPlayer Black:\x1b[0m ";
+    cout << "\x1b[0m\x1b[40;37m1.Player Black:\x1b[0m ";
     cout << (waiting_user_existance[0] ? waiting_username[0] : "(empty)");
     if(waiting_user_existance[0]) cout << MOVE(4, 16 + ready_pos) << (waiting_is_ready[0] ? "  (ready)" : "(unready)");
     cout << endl;
-    cout << "\x1b[0m\x1b[30;47mPlayer White:\x1b[0m ";
+    cout << "\x1b[0m\x1b[30;47m2.Player White:\x1b[0m ";
     cout << (waiting_user_existance[1] ? waiting_username[1] : "(empty)");
     if(waiting_user_existance[1]) cout << MOVE(5, 16 + ready_pos) << (waiting_is_ready[1] ? "  (ready)" : "(unready)");
     cout << endl;
-    cout << "\x1b[0m\x1b[40;37mObserver 1  :\x1b[0m ";
+    cout << "\x1b[0m\x1b[40;37m3.Observer 1  :\x1b[0m ";
     cout << (waiting_user_existance[2] ? waiting_username[2] : "(empty)") << endl;
-    cout << "\x1b[0m\x1b[40;37mObserver 2  :\x1b[0m ";
+    cout << "\x1b[0m\x1b[40;37m4.Observer 2  :\x1b[0m ";
     cout << (waiting_user_existance[3] ? waiting_username[3] : "(empty)") << endl;
-    cout << "\x1b[0m\x1b[40;37mObserver 3  :\x1b[0m ";
+    cout << "\x1b[0m\x1b[40;37m5.Observer 3  :\x1b[0m ";
     cout << (waiting_user_existance[4] ? waiting_username[4] : "(empty)") << endl;
+    cout << "════════════════════════════════════════" << endl;
+    cout << "Press Esc to leave room\nPress R to ready/unready\n" << flush;
+    cout << "Press Number 1~5 to change position\n";
 }
