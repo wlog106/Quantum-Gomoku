eec83bc54d5c9d0d77f354abdff4f157423d647d
wlog106 <xuanhao8765@gmail.com>
2025-12-06 21:29:09 +0800

    refactor: new processors in server structure

--- server_/logic/processor.cpp
+++ server_/logic/processor.cpp
@@ -1,30 +1,48 @@
 #include "../server_.h"
 #include <cstring>
+#include <sys/epoll.h>
 
 void processor(
     ServerContext *scxt, 
-    conn_t *u, 
-    std::queue<string> &q
+    ServerObjects *sobj,
+    conn *u, 
+    std::queue<std::pair<int, char*>> &q
 ){
-    std::queue<job_t> jobs;
-    int cmd_id = std::stoi(q.front());
-    q.pop();
-    switch(cmd_id)
-    {
-        case (C_create_new_account):
-            u->name = q.front();
-            q.pop();
-            strcpy(u->hash, q.front().data());
-            q.pop();
-            jobs.push(job_t(DB_SIGNUP, u));
-            break;
-        case (C_login_to_server):
-            u->name = q.front();
-            q.pop();
-            strcpy(u->hash, q.front().data());
-            q.pop();
-            jobs.push(job_t(DB_LOGIN, u));
-            break;
+    while(!q.empty()){
+        job_t *newJob = new job_t;
+        char *cmd = (char*)malloc(MAXLINE*sizeof(char));
+        switch(q.front().first)
+        {
+            case (C_create_new_account):
+                sscanf(q.front().second, "%s %s", u->name, u->hash);
+                u->db_job_pending = 1;
+                newJob->type = DW_SIGNUP;
+                sprintf(cmd, "%d %d %s\n", DW_SIGNUP, u->fd, q.front().second);
+                newJob->line = cmd;
+                newJob->r_ptr = cmd;
+                newJob->len = strlen(cmd);
+                sobj->dwq->push(newJob);
+                if(sobj->dwq->size()==1) goto add_dw_to_epoll;
+                break;
+                
+            case (C_login_to_server):
+                sscanf(q.front().second, "%s %s", u->name, u->hash);
+                u->db_job_pending = 1;
+                newJob->type = DW_LOGIN;
+                sprintf(cmd, "%d %d %s\n", DW_LOGIN, u->fd, q.front().second);
+                newJob->line = cmd;
+                newJob->r_ptr = cmd;
+                newJob->len = strlen(cmd);
+                sobj->dwq->push(newJob);
+                if(sobj->dwq->size()==1) goto add_dw_to_epoll;
+                break;
+
+            add_dw_to_epoll:
+                struct epoll_event ev;
+                ev.events = EPOLLOUT;
+                ev.data.fd = scxt->dw_fd;
+                Epoll_ctl(scxt->epfd, EPOLL_CTL_MOD, scxt->dw_fd, &ev);
+        }
+        q.pop();
     }
-    dispatcher(scxt, jobs);
 }
