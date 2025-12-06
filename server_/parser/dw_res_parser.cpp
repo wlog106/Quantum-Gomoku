daa639492086fae39ab23b874a9ab41c2d0adcdf
wlog106 <xuanhao8765@gmail.com>
2025-12-06 21:30:04 +0800

    refactor: new parsers in server structure

---
+++ server_/parser/dw_res_parser.cpp
@@ -0,0 +1,29 @@
+#include "../server_.h"
+
+void dw_res_parser(
+    ServerContext *scxt,
+    ServerObjects *sobj
+){  
+    char *line;
+    int id, fd;
+    dw_res newDwres;
+    std::queue<dw_res> q;
+    /*
+        database worker response format:
+        > "fd id type:result"
+        dw_res_parser decode it and give
+        dw_res_processor the following info:
+        > conn *u, int type, int result
+    */
+    while((
+        line = sobj->dwr_buf->getline())!=NULL
+    ){
+        sscanf(line, "%d %d %d:%d",
+               &fd, &id, &newDwres.type, &newDwres.result);
+        newDwres.u = get_user(fd, sobj->fd_to_conn);
+        newDwres.u->id = id;
+        q.push(newDwres);
+        free(line);
+    }
+    dw_res_processor(scxt, q);
+}
